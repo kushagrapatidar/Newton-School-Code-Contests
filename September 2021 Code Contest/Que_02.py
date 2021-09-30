e79a36f022857e95be48f9d9fef2c2dda70d61a7
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-09-30 21:42:23 +0530

    main

--- September 2021 Code Contest/Que_02.py
+++ September 2021 Code Contest/Que_02.py
@@ -1,12 +1,14 @@
-N=int(input())
-set=[]
-for _ in range(N):
-    set.append(int(input()))
-set.sort()
-subset=[]
-for _ in set:
-    if _//2 not in subset:
-        subset.append(_)
-print(len(subset))
-for _ in subset:
-    print(_)
+T=int(input())
+C=[]
+for i in range(T):
+    C.append(int(input()))
+for N in C:  
+    set=[]
+    for _ in range(1,N+1):
+        set.append(_)
+    set.sort()
+    subset=[]
+    for _ in set:
+        if _/2 not in subset:
+            subset.append(_)
+    print(len(subset))
