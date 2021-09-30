6c14e959e5d2c6e783bb0cd197439fa15011f4f0
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-09-30 22:40:34 +0530

    main

--- September 2021 Code Contest/Que_02.py
+++ September 2021 Code Contest/Que_02.py
@@ -1,16 +1,12 @@
-T=int(input())
-lensubsets=[]
-for i in range(T):
-    N=int(input())
-    set=[]
-    for _ in range(N):
-        set.append(_+1)
-    set.sort()
-    subset=[]
-    for _ in set:
-        if _/2 not in subset:
-            subset.append(_)
-    lensubsets.append(len(subset))
-
-for _ in lensubsets:
+n=int(input())
+len_sub_set=[]
+for i in range(n):
+    num=int(input())
+    count=0
+    sub_set=[]
+    for j in range(num):
+        if (j+1)/2 not in sub_set:
+            sub_set.append(j+1)
+    len_sub_set.append(len(sub_set))
+for _ in len_sub_set:
     print(_)
---
+++ September 2021 Code Contest/Que_03.py
@@ -0,0 +1,8 @@
+N=int(input())
+cordinates=[]
+for _ in range(N):
+    x,y=input().split
+    cordinates.append((int(x),int(y)))
+i=0
+while i<len(cordinates)-1:
+    while i<len(cordinates)-1:
