ffbe6293498296ba1103191b9e9bcf3624b17979
Kushagra Patidar <kushagra24ai027@satiengg.in>
2021-10-18 22:50:34 +0530

    Update Que_02.py

--- September 2021 Code Contest/Que_02.py
+++ September 2021 Code Contest/Que_02.py
@@ -1,12 +1,17 @@
-n=int(input())
+def func(nums,len_sub_set): 
+    if len(nums)>0:
+        sub_set=[]
+        for j in range(1,nums[0]+1):
+            if j/2 not in sub_set:
+                sub_set.append(j)
+        len_sub_set.append(len(sub_set))
+        len_sub_set=func(nums[1:],len_sub_set)
+    return len_sub_set
+
 len_sub_set=[]
-for i in range(n):
-    num=int(input())
-    count=0
-    sub_set=[]
-    for j in range(num):
-        if (j+1)/2 not in sub_set:
-            sub_set.append(j+1)
-    len_sub_set.append(len(sub_set))
+nums=[]
+for i in range(int(input())):
+    nums.append(int(input()))
+len_sub_set=func(nums,len_sub_set)
 for _ in len_sub_set:
     print(_)
